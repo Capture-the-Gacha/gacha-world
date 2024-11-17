6396d443488830c26b16be64ece01f046645424a
Samuele Franchi <franchisamuele@gmail.com>
2024-11-17 16:13:19 +0100

    Removed useless dbm + refactoring

---
+++ services/Player/model.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+from fastapi import Depends
+from sqlmodel import Field, Session, SQLModel
+from datetime import datetime
+
+def get_current_timestamp():
+    return int(datetime.now().timestamp())
+
+class Player(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    username: str = Field(index=True)
+    password: str = Field()
+    balance: float = Field()
+
+class Recharge(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    player_id: int = Field(foreign_key="player.id")
+    amount: float = Field()
+    timestamp: int = Field(default_factory=get_current_timestamp)
+
+class RechargePublic(SQLModel):
+    amount: float
+    timestamp: int
+
+class Collection(SQLModel, table=True):
+    player_id: int = Field(foreign_key="player.id", primary_key=True)
+    gacha_id: int = Field(primary_key=True)
+    quantity: int = Field()
+
+class CollectionPublic(SQLModel):
+    gacha_id: int
+    quantity: int
+
+class Roll(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    player_id: int = Field(foreign_key="player.id")
+    gacha_id: int = Field()
+    paid_price: float = Field()
+    timestamp: int = Field(default_factory=get_current_timestamp)
+
+class RollPublic(SQLModel):
+    gacha_id: int
+    paid_price: float
+    timestamp: int
+
+def create_db_and_tables(engine):
+    SQLModel.metadata.create_all(engine)
+
+def get_session(engine):
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
