24bce5af0c60faba69fd5b4c60a56b8513e00d90
lorenzobandini <lboa@hotmail.it>
2024-12-06 00:01:03 +0100

    first mock-up admin gateway

--- services/AdminGateway/app.py
+++ services/AdminGateway/app.py
@@ -1,22 +1,61 @@
-from fastapi import FastAPI
+import uvicorn, os
 from dotenv import load_dotenv
-import os
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+import httpx
 
 load_dotenv()
 
 CERT_PATH = os.getenv('CERT_PATH')
 KEY_PATH = os.getenv('KEY_PATH')
 
+PORT = int(os.getenv('PORT'))
 ADMIN_PORT= int(os.getenv('ADMIN_PORT'))
+
+GACHA_HOST = os.getenv('GACHA_HOST')
+AUTH_HOST = os.getenv('AUTH_HOST')
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Admin Gateway is running"}
+async def forward(request: Request, url: str):
+    async with httpx.AsyncClient(verify=False) as client:
+        response = await client.request(
+            method=request.method, 
+            url=url, 
+            content=await request.body(), 
+            headers=request.headers
+        )
+
+    return Response(content=response.content, status_code=response.status_code, headers=response.headers)
+
+
+# === Auth ===
+
+# TODO
+
+# === Gacha ===
+
+@app.get('/gachas')
+async def getCollection(request: Request):
+    return await forward(request, f'https://{GACHA_HOST}:{PORT}/gachas')
+
+@app.get('/gachas/{gacha_id}')
+async def getCollection(request: Request, gacha_id: int):
+    return await forward(request, f'https://{GACHA_HOST}:{PORT}/gachas/{gacha_id}')
+
+@app.post('/gachas')
+async def addGacha(request: Request):
+    return await forward(request, f'https://{GACHA_HOST}:{PORT}/gachas')
+
+@app.put('/gachas/{gacha_id}')
+async def updateGacha(request: Request, gacha_id: int):
+    return await forward(request, f'https://{GACHA_HOST}:{PORT}/gachas/{gacha_id}')
+
+@app.delete('/gachas/{gacha_id}')
+async def deleteGacha(request: Request, gacha_id: int):
+    return await forward(request, f'https://{GACHA_HOST}:{PORT}/gachas/{gacha_id}')
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(
         app, 
         host="0.0.0.0", 
